03d8dc34e878c5c51ead6dde90142d31236d12d9
Pedro Old <pedrohenrique.santos@corabiz.ag>
2026-04-19 00:04:08 -0300

    feat(validator): validator keys configuration

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,7 +1,21 @@
 import sys
+from typing import Set
 
 
 def is_valid_keys(configs: dict) -> bool:
+    mandatory_keys: Set[str] = {
+        "WIDTH",
+        "HEIGHT",
+        "ENTRY",
+        "EXIT",
+        "OUTPUT_FILE",
+        "PERFECT"
+    }
+    current_keys: Set[str] = set(configs.keys())
+    missing_keys: Set[str] = mandatory_keys - current_keys
+    if missing_keys:
+        print(f"Erro: Chaves obrigatórias ausentes: {', '.join(missing_keys)}")
+        return False
     return configs.__contains__("WIDTH")
 
 
