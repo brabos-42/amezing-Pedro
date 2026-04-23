0d0e9874399a37a04a1a651942ec7a93ae604a34
pdo-sant <pedro.santosvale1999@gmail.com>
2026-04-23 15:01:02 -0300

    aqui

---
+++ utils/read_configuration.py
@@ -0,0 +1,25 @@
+import sys
+from is_valid_keys import is_valid_keys
+
+
+def read_configuration(path: str) -> dict:
+    configuration: dict = {}
+    try:
+        with open(path, "r") as file:
+            for line in file:
+                line = line.strip()
+                if (not line or line.startswith("#")):
+                    continue
+                if ("=" not in line):
+                    print("Erro: Sintaxe inválida")
+                    sys.exit(1)
+                key, value = line.split("=", 1)
+                key, value = key.strip(), value.strip()
+                configuration[key] = value
+        if (not is_valid_keys(configuration)):
+            print("Not has all atributes mandatorys")
+            return None
+        return (configuration)
+    except FileNotFoundError:
+        print("Erro open the file")
+        return None
