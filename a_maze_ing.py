5bee9a1f436b5d9d142bccf740d9995cec02d9c4
lxavier- <ryuuwatanabe2612@gmail.com>
2026-04-29 15:46:41 -0300

    seed

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -23,6 +23,7 @@ class ValuesConfg:
     perfect: bool
     entry: tuple
     exit: tuple
+    seed: str
 
 
 def parse_bool(value: str) -> bool:
@@ -45,9 +46,15 @@ def main() -> None:
         perfect=parse_bool(values_config['PERFECT']),
         entry=tuple(values_config['ENTRY']),
         exit=tuple(values_config['EXIT']),
-        display_maze=parse_bool(values_config['DISPLAY_MAZE'])
+        display_maze=parse_bool(values_config['DISPLAY_MAZE']),
+        seed=str(values_config['SEED'])
     )
-    random.seed(42)
+    if (valuesReceiver.seed == "false" or
+       valuesReceiver.seed == "" or
+       valuesReceiver.seed == "False"):
+        random.seed()
+    else:
+        random.seed(valuesReceiver.seed)
     test = backtracking(valuesReceiver.width,
                         valuesReceiver.height,
                         valuesReceiver.path,
